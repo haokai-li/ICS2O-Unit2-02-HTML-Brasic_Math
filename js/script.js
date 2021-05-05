472e0bfb2ad76713d381419edce3c28f82cdebfc
haokaili <>
2021-05-05 03:26:42 +0000

    added math

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: haokai
+// Created on: May 2021
+// This is the Brasic_Math
+
+function doMathClicked() {
+  // this function does Brasic_Math
+
+  document.getElementById('multiply-math').innerHTML = 'The area is:' + (5 * 3) + 'cm²'
+  document.getElementById('add-math').innerHTML = 'The perimeter is:'+ (2 * (5 + 3)) + 'cm'
+}
